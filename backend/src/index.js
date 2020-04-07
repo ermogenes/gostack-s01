0b91203abc9735a3d456f1409871e25f4a11ff0e
Ermogenes Palacio <ermogenes@gmail.com>
2020-04-07 12:20:32 -0300

    Add projects CRUD routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,36 @@ app.get('/', (request, response) => {
     });
 });
 
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3 (supostamente novo)',
+    ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 4  (supostamente alterado)',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
 app.listen(3333, () => {
     console.log("👌 back-end started");
 });
