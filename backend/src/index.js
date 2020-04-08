c3b64a04b27b9b6a8c0466815d71337057fbd104
Ermogenes Palacio <ermogenes@gmail.com>
2020-04-08 10:34:48 -0300

    Add logging and validation middlewares

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,46 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
 const projects = [];
+
+function logRequest(request, response, next) {
+    const { method, url } = request;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+    console.time(logLabel);
+
+    next();
+
+    console.timeEnd(logLabel);
+}
+
+function logCreation(request, response, next) {
+    const { title } = request.body;
+
+    const logLabel = `"${title}" created.`;
+    console.log(logLabel);
+
+    return next();
+}
+
+function validateProjectId(request, response, next) {
+    const { id } = request.params;
+
+    if (!isUuid(id)) {
+        return response.status(400).json({
+            error: 'Invalid project id.',
+        });
+    }
+
+    return next();
+}
+
+app.use(logRequest);
+app.use('/project/:id', validateProjectId);
 
 app.get('/', (request, response) => {
     return response.json({
@@ -29,7 +64,7 @@ app.get('/projects', (request, response) => {
     return response.json(results);
 });
 
-app.post('/projects', (request, response) => {
+app.post('/projects', logCreation, (request, response) => {
     const { title, owner } = request.body;
 
     const project = { 
