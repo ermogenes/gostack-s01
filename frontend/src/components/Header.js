404b8cb7d5068537920989e5d1b3772b05861716
Ermogenes Palacio <ermogenes@gmail.com>
2020-04-09 16:39:20 -0300

    Use JSX fragment

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function Header() {
     return (
+        <>
         <header>ReactJs</header>
+        <div>The number one framework!</div>
+        </>
     );
 }
