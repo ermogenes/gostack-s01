ac116b2a1da349d177f0a7edc56ae7453d99d890
Ermogenes Palacio <ermogenes@gmail.com>
2020-04-09 16:21:58 -0300

    Render a JSX element

--- frontend/src/add.js
+++
@@ -1,4 +0,0 @@
-// Arrow function will be transpiled by babel
-// export will be loaded by webpack
-
-export const add = (a, b) => { return a + b };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,7 @@
-import { add } from './add';
+import React from 'react';
+import { render } from 'react-dom';
 
-console.log(add(1, 4));
+render(
+    <h1>Hello World</h1>,
+    document.getElementById('app')
+);
