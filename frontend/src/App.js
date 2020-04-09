714878d35ad38823c8430c3810af9f37be009364
Ermogenes Palacio <ermogenes@gmail.com>
2020-04-09 17:41:43 -0300

    Use states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+    const [ courses, setCourses ] = useState(['OmniStack', 'GoStack']);
+
+    function handleAddCourse() {
+        setCourses([
+            ...courses,
+            `Course in ${Date.now()}`,
+        ]);
+    }
+
     return (
         <>
         <Header title="ReactJS" lead="The number one framework!" />
         <Header title="Rocketseat courses" lead="Yes!">
             <ul>
-                <li>OmniStack</li>
-                <li>GoStack</li>
+                {courses.map(course => <li key={course}>{course}</li>)}
             </ul>
         </Header>
+
+        <button type="button" onClick={handleAddCourse}>Add course</button>
         </>
     );
 }
