3861b776e94d7aa94e65e308f1212b6a830f8702
Ermogenes Palacio <ermogenes@gmail.com>
2020-04-13 14:52:27 -0300

    Hello world

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
 export default function App() {
-    return <View />;
+    return (
+        <>
+            <StatusBar barStyle='light-content' backgroundColor='#7159c1' />
+            <View style={styles.container}>
+                <Text style={styles.bigText}>Hello World!</Text>
+            </View>
+        </>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#7159c1',
+    },
+    bigText: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#fff',
+    },
+});
